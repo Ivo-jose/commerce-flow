bacb78b373986fecf288c3790de54a2cd3ca8649
Ivo Goncalves <ivojmgs19@gmail.com>
2026-08-25 15:42:12 -0300

    chore: initialize commerce-flow monorepo

---
+++ services/catalog-service/src/test/java/br/com/ivogoncalves/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ivogoncalves.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
